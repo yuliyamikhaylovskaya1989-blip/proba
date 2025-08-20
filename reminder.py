3dee1ec1fae21217a88c4b9375d6575792b39cd8
Юлия <yuliyamikhaylovskaya1989@gmail.com>
2025-08-20 15:57:42 +0300

    создаем главное окно

---
+++ reminder.py
@@ -0,0 +1,52 @@
+from tkinter import *
+from tkinter import simpledialog as sd
+from tkinter import messagebox as mb
+import datetime
+import pygame
+import time
+
+
+t = None
+
+
+def set():
+    global t
+    rem = sd.askstring("Время напоминания", "Введите время в формате ЧЧ:ММ
+    (24-часовой формат)")
+    if rem:
+        try:
+        hour = int(rem.split(":")[0])
+        minute = int(rem.split(":")[1])
+        now = datetime.datetime.now()
+        print(now)
+        dt = now.replace(hour=hour, minute=minute)
+        print(dt)
+        t = dt.timestamp()
+        print(t)
+        label.config(text=f"Напоминание установлено на:
+                     {hour:02}:{minute:02}")
+        except ValueError:
+        mb.showerror("Ошибка", "Неверный формат времени")
+
+
+def check():
+global t
+if t:
+now = time.time()
+if now >= t:
+play_snd()
+t = None
+window.after(10000, check)
+def play_snd():
+    pygame.mixer.init()
+    pygame.mixer.music.load("reminder.mp3")
+    pygame.mixer.music.play()
+
+window = Tk()
+window.title("Напоминание")
+label = Label(text="Установите напоминание", font=("Arial", 14))
+label.pack(pady=10)
+set_button = Button(text="Установить напоминание", command=set)
+set_button.pack(pady=10)
+check()
+window.mainloop()
